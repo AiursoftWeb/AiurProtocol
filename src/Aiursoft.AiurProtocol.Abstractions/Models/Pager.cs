9f61996fed83feb97fd3eabca97f12f08e8d55e6
Anduin Xue <anduin@aiursoft.com>
2023-06-23 12:40:00 +0000

    AiurProtocol add default page number and size.

--- src/Aiursoft.AiurProtocol.Abstractions/Models/Pager.cs
+++ src/Aiursoft.AiurProtocol.Abstractions/Models/Pager.cs
@@ -9,12 +9,12 @@ public abstract class Pager
     /// </summary>
     [Range(1, 100)]
     [Required(ErrorMessage = "Please provide the page size!")]
-    public int PageSize { get; set; }
+    public int PageSize { get; init; } = 10;
 
     /// <summary>
     /// The page number of the grouped items. Starts with 1.
     /// </summary>
     [Range(1, int.MaxValue)]
     [Required(ErrorMessage = "Please provide the page number!")]
-    public int PageNumber { get; set; }
+    public int PageNumber { get; init; } = 1;
 }
